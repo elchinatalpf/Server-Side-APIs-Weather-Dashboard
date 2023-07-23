dd22662e43388ce85799fdc86836c22bb896979a
Javier <elchinatalpf@github.com>
2023-07-23 16:49:24 -0500

    Finished structure

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // 41fa68e7995dacccf780b9109022414d
 // email api key: f74896a5d35555f0459e455e5e04f3e2
-var apiOpenWeatherKey = 'f74896a5d35555f0459e455e5e04f3e2';
+var apiKey = 'f74896a5d35555f0459e455e5e04f3e2';
+
+
 
 // 1. Show curentTime
 // 2. Create an event click for search cities
