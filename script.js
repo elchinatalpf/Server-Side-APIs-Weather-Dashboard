152f1e0ccc5317a4be0626d9a9d00830af7ab53a
Javier <elchinatalpf@github.com>
2023-07-23 14:41:02 -0500

    Deleted library and organize CSS

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 // email api key: f74896a5d35555f0459e455e5e04f3e2
 var apiOpenWeatherKey = 'f74896a5d35555f0459e455e5e04f3e2';
 
+// 1. Show curentTime
+// 2. Create an event click for search cities
+// 3. Pull cities and show current and 5 days forscast
+// 4. Safe search in LocalStorage
+// 5. Render last search from Local Storate
+// 6. Fetch URL probably with jQuery (ajax)
+// 7. 
